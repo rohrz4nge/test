c4e21c1652e129702725e0093545e16ff2526607
maximilianlohmann <maximilian.lohmann.hh@gmail.com>
2020-11-08 20:10:16 +0100

    use new camera library

--- packages/firebase_ml_vision/example/lib/main.dart
+++ packages/firebase_ml_vision/example/lib/main.dart
@@ -2,14 +2,16 @@ import 'dart:async';
 import 'dart:ffi';
 import 'dart:io';
 
-import 'package:camera/camera.dart';
+import 'package:camerawesome/models/orientations.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:image_picker/image_picker.dart';
 import 'painter.dart';
 import 'package:image/image.dart' as test;
+
 //import 'package:exif/exif.dart';
 //import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:camerawesome/camerawesome_plugin.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:http/http.dart' as http;
 import "package:convert/convert.dart";
@@ -26,37 +28,66 @@ void logError(String code, String message) =>
 
 class _CameraExampleHomeState extends State<CameraExampleHome>
     with WidgetsBindingObserver {
-  CameraController controller;
-  CameraDescription description;
   String imagePath;
-  final ImagePicker _picker = ImagePicker();
+  ValueNotifier<CameraFlashes> _switchFlash;
+  ValueNotifier<double> zoomNotifier;
+  ValueNotifier<Sensors> sensor;
+  ValueNotifier<Size> _photoSize;
+  PictureController controller;
+  bool _inactive = false;
+  double _previousScale = 1;
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addObserver(this);
-    onNewCameraSelected();
     update_db();
+
+    _switchFlash = ValueNotifier(CameraFlashes.NONE);
+    zoomNotifier = ValueNotifier(0);
+
+    sensor = ValueNotifier(Sensors.BACK);
+    _photoSize = ValueNotifier(null);
+    controller = PictureController();
+    _previousScale = 1;
   }
 
   @override
   void dispose() {
     WidgetsBinding.instance.removeObserver(this);
     super.dispose();
+      _switchFlash.dispose();
+      _photoSize.dispose();
+      sensor.dispose();
+      zoomNotifier.dispose();
+      print("disposing");
+      super.dispose();
+  }
+
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
   }
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     // App state changed before we got the chance to initialize.
-    if (controller == null || !controller.value.isInitialized) {
+    if (controller == null) {
       return;
+      _switchFlash.dispose();
+      _photoSize.dispose();
+      sensor.dispose();
+      zoomNotifier.dispose();
     }
     if (state == AppLifecycleState.inactive) {
-      controller?.dispose();
+      setState(() {
+        _inactive = true;
+      });
     } else if (state == AppLifecycleState.resumed) {
-      if (controller != null) {
-        onNewCameraSelected();
-      }
+      setState(() {
+        //_inactive = false;
+      });
     }
   }
 
@@ -76,9 +107,8 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
     return url;
   }
 
-
   Future<bool> check_for_db_change() async {
-    String last_change_filename =  "last_change";
+    String last_change_filename = "last_change";
     String last_change_url = "$_url/$last_change_filename";
     String last_change;
     try {
@@ -88,7 +118,7 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
       return true;
     }
     String last_remote_change;
-    try{
+    try {
       last_remote_change = await http.read(last_change_url);
     } on SocketException catch (_) {
       print("Network error, no change found.");
@@ -98,7 +128,7 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
   }
 
   Future<void> update_db() async {
-    String last_change_filename =  "last_change";
+    String last_change_filename = "last_change";
     String last_change_url = "$_url/$last_change_filename";
     String db_filename = "sortedIngredients.json";
     String db_url = "$_url/$db_filename";
@@ -109,8 +139,8 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
     String last_remote_change;
     http.Response db_response;
     try {
-       last_remote_change = await http.read(last_change_url);
-       db_response = await http.get(db_url);
+      last_remote_change = await http.read(last_change_url);
+      db_response = await http.get(db_url);
     } on SocketException catch (_) {
       print("Network error, not updating.");
       return;
@@ -123,8 +153,6 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
       File last_change_file = await get_local_file(last_change_filename);
       last_change_file.writeAsString(last_remote_change);
     }
-
-
   }
 
   Future<String> getImagePath() async {
@@ -138,41 +166,18 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
 
   Future<String> fixExifRotation(String imagePath) async {
     return imagePath;
-    /*final originalFile = File(imagePath);
-    List<int> imageBytes = await originalFile.readAsBytes();
-    await FlutterImageCompress.compressWithFile(
-      originalFile.absolute.path,
-      quality: 90,
-      minWidth: 1024,
-      minHeight: 1024,
-      rotate: 90,
-    );
-    print("ßßßßßßßß");
-
-    // We'll use the exif package to read exif data
-    // This is map of several exif properties
-    // Let's check 'Image Orientation'
-    final exifData = await readExifFromBytes(imageBytes);
-
-    File fixedImage;
-
-    if (exifData['Image Orientation'].printable.contains('Horizontal')) {
-      fixedImage = await FlutterImageCompress.compressAndGetFile(
-          imagePath, await getImagePath(),
-          quality: 100, rotate: 90);
-    } else if (exifData['Image Orientation'].printable.contains('180')) {
-      fixedImage = await FlutterImageCompress.compressAndGetFile(
-          imagePath, await getImagePath(),
-          quality: 100, rotate: -90);
-    } else {
-      fixedImage = await FlutterImageCompress.compressAndGetFile(
-          imagePath, await getImagePath(),
-          quality: 100, rotate: 0);
-    }
-    return fixedImage.path;*/
   }
 
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  Widget cameraGetter() {
+    if (_inactive) {
+      setState(() {
+        _inactive = false;
+      });
+    }
+    return _cameraPreviewWidget();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -187,7 +192,7 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
               child: Padding(
                 padding: const EdgeInsets.all(1.0),
                 child: Center(
-                  child: _cameraPreviewWidget(),
+                  child: cameraGetter(),
                 ),
               ),
             ),
@@ -199,24 +204,52 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
         shape: CircularNotchedRectangle(),
         child: Container(
           height: 75,
-          padding: const EdgeInsets.fromLTRB(10.0,0,10,0),
+          padding: const EdgeInsets.fromLTRB(10.0, 0, 10, 0),
           child: Row(
             mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              IconButton(
-                icon: Icon(Icons.photo_library),
-                onPressed: () async {
-                  /*final String imagePath =
+              Expanded(
+                child: IconButton(
+                  icon: Icon(Icons.photo_library),
+                  onPressed: () async {
+                    /*final String imagePath =
                       (await _picker.getImage(source: ImageSource.gallery)).path;*/
-                  final String imagePath = (await FilePicker.platform
-                              .pickFiles(type: FileType.image))
-                          .files
-                          .single
-                          .path;
-                  print(imagePath);
-                  await getAnalyzeView(context, imagePath);
-                },
+                    final String imagePath = (await FilePicker.platform
+                            .pickFiles(type: FileType.image))
+                        .files
+                        .single
+                        .path;
+                    print(imagePath);
+                    await getAnalyzeView(context, imagePath);
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(),
+                flex: 2,
+              ),
+              Expanded(
+                child: IconButton(
+                  icon: Icon(_getFlashIcon()),
+                  onPressed: () async {
+                    switch (_switchFlash.value) {
+                      case CameraFlashes.NONE:
+                        _switchFlash.value = CameraFlashes.ON;
+                        break;
+                      case CameraFlashes.ON:
+                        _switchFlash.value = CameraFlashes.AUTO;
+                        break;
+                      case CameraFlashes.AUTO:
+                        _switchFlash.value = CameraFlashes.ALWAYS;
+                        break;
+                      case CameraFlashes.ALWAYS:
+                        _switchFlash.value = CameraFlashes.NONE;
+                        break;
+                    }
+                    setState(() {});
+                  },
+                ),
               )
             ],
           ),
@@ -227,13 +260,13 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
         width: 65.0,
         child: FittedBox(
           child: FloatingActionButton(
-              onPressed: controller != null && controller.value.isInitialized
+              onPressed: controller != null
                   ? onTakePictureButtonPressed
                   : null,
               child: Icon(Icons.camera),
               backgroundColor: Colors.blue
-            // ...FloatingActionButton properties...
-          ),
+              // ...FloatingActionButton properties...
+              ),
         ),
       ),
 
@@ -243,80 +276,60 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
     );
   }
 
+  IconData _getFlashIcon() {
+    switch (_switchFlash.value) {
+      case CameraFlashes.NONE:
+        return Icons.flash_off;
+      case CameraFlashes.ON:
+        return Icons.flash_on;
+      case CameraFlashes.AUTO:
+        return Icons.flash_auto;
+      case CameraFlashes.ALWAYS:
+        return Icons.highlight;
+      default:
+        return Icons.flash_off;
+    }
+  }
+
   /// Display the preview from the camera (or a message if the preview is not available).
   Widget _cameraPreviewWidget() {
-    if (controller == null || !controller.value.isInitialized) {
-      return const Text(
-        'No camera found',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 24.0,
-          // fontWeight: FontWeight.w900,
-        ),
-      );
-    } else {
-      return AspectRatio(
-        aspectRatio: controller.value.aspectRatio,
-        child: CameraPreview(controller),
-      );
+    return GestureDetector(
+      onScaleStart: (ScaleStartDetails details) {
+      //print(details);
+      // Does this need to go into setState, too?
+      // We are only saving the scale from before the zooming started
+      // for later - this does not affect the rendering...
+      _previousScale = zoomNotifier.value + 1;
+    },
+    onScaleUpdate: (ScaleUpdateDetails details) {
+    //print(details.scale);
+      double result = _previousScale * (details.scale) - 1;
+    if (result < 1 && result > 0) {
+      zoomNotifier.value = result;
     }
+    setState(() {});
+    },
+      child: CameraAwesome(
+        testMode: false,
+        onPermissionsResult: (bool result) {},
+        selectDefaultSize: (availableSizes) {
+          if (availableSizes.length == 1) return availableSizes[0];
+          return availableSizes[1];
+        },
+        onCameraStarted: () {print("started");},
+        zoom: zoomNotifier,
+        sensor: sensor,
+        photoSize: _photoSize,
+        switchFlashMode: _switchFlash,
+        fitted: false,
+      ),
+    );
   }
 
   String timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
 
   void showInSnackBar(String message) {
     _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(message)));
-  }
-
-  void onNewCameraSelected() async {
-    if (controller != null) {
-      await controller.dispose();
-    }
-    List<CameraDescription> cameras = await availableCameras();
-    if (cameras.length == 0) {
-      return;
-    }
-    controller = CameraController(
-      (await availableCameras()).first,
-      ResolutionPreset.ultraHigh,
-      enableAudio: false,
-    );
-
-    // If the controller is updated then update the UI.
-    controller.addListener(() {
-      if (mounted) setState(() {});
-      if (controller.value.hasError) {
-        showInSnackBar('Camera error ${controller.value.errorDescription}');
-      }
-    });
-
-    try {
-      await controller.initialize();
-      /*controller.startImageStream((image) async {
-        //print(image.runtimeType);
-
-        final FirebaseVisionImageMetadata metadata = FirebaseVisionImageMetadata(
-            rawFormat: image.format.raw,
-            size: Size(image.width.toDouble(),image.height.toDouble()),
-            planeData: image.planes.map((currentPlane) => FirebaseVisionImagePlaneMetadata(
-                bytesPerRow: currentPlane.bytesPerRow,
-                height: currentPlane.height,
-                width: currentPlane.width
-            )).toList(),
-            rotation: ImageRotation.rotation90
-        );
-        FirebaseVisionImage tmp = FirebaseVisionImage.fromBytes(image.planes[0].bytes, metadata);
-        VisionText x = await _recognizer.processImage(tmp);
-        print(x.text);
-
-      });*/
-    } on CameraException catch (e) {
-      _showCameraException(e);
-    }
-
-    if (mounted) {
-      setState(() {});
-    }
   }
 
   Future getAnalyzeView(BuildContext context, String filePath) {
@@ -351,20 +364,18 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
   }
 
   Future<String> takePicture() async {
-    if (!controller.value.isInitialized) {
+    if (controller == null) {
       showInSnackBar('Error: no camera found.');
       return null;
     }
-
-    if (controller.value.isTakingPicture) {
-      return null;
-    }
+    // todo check for image already being taken
 
     String tmpPath = await getImagePath();
     try {
+      //await controller.takePicture(tmpPath);
       await controller.takePicture(tmpPath);
       tmpPath = await fixExifRotation(tmpPath);
-    } on CameraException catch (e) {
+    } on Exception catch (e) {
       _showCameraException(e);
       return null;
     }
@@ -372,14 +383,15 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
     test.Image orientedImage = test.bakeOrientation(image);
     // Save the thumbnail as a PNG.
 
-    await File(tmpPath + '.png')
-      ..writeAsBytesSync(test.encodePng(orientedImage));
-    return tmpPath + '.png';
+    await File(tmpPath + '.jpg')
+      ..writeAsBytesSync(test.encodeJpg(orientedImage));
+    return tmpPath + '.jpg';
   }
 
-  void _showCameraException(CameraException e) {
-    logError(e.code, e.description);
-    showInSnackBar('Error: ${e.code}\n${e.description}');
+  void _showCameraException(Exception e) {
+    //logError(e.code, e.description);
+    //showInSnackBar('Error: ${e.code}\n${e.description}');
+    print("Error when taking image");
   }
 }
 
@@ -392,15 +404,9 @@ class CameraApp extends StatelessWidget {
   }
 }
 
-List<CameraDescription> cameras = [];
 
 Future<void> main() async {
   // Fetch the available cameras before initializing the app.
-  try {
-    WidgetsFlutterBinding.ensureInitialized();
-    cameras = await availableCameras();
-  } on CameraException catch (e) {
-    logError(e.code, e.description);
-  }
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(CameraApp());
 }
